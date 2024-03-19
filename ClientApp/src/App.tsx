b56da0ebef19ddb937ec760dbf73b18f5f7c22ef
Fernando Aguilar <sparkmanperspective@gmail.com>
2024-03-19 17:45:35 -0400

    npm installed bulma and imported the css framework into the front-end.

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import 'bulma/css/bulma.css'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div className="container">
+      <h1 className="title has-text-centered">Welcome to Blockbuster Video</h1>
+      <div className="notification is-primary">
+        This is primary notification
+      </div>
+    </div>
+  )
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Mar. 18, 2024
 
-This will be a rough draft of the front-end side of my final project. Using Bulma.io framework.
+I will be integrating the Bulma CSS framework with this React application. https://bulma.io
